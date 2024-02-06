8413fb9b2db750f9a541fdd32f7a021204cd8229
Geoermeidis <75807347+Geoermeidis@users.noreply.github.com>
2024-02-06 11:38:48 +0200

    Changed get incidents response

---
+++ Models/DTO/IncidentResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartAlertAPI.Models.DTO
+{
+    public class IncidentResponseDTO
+    {
+        public string CategoryName { get; set; } = string.Empty;
+        public string Comments { get; set; } = string.Empty;
+        public string SubmittedByUsername { get; set; } = string.Empty;
+        public string PhotoURL { get; set; } = string.Empty;
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string SubmittedAt { get; set; } = string.Empty;
+        public int TotalSubmissions { get; set; } = 0;
+        public IncidentState State { get; set; } = IncidentState.Submitted;
+    }
+}
--- Utils/MappingConfig.cs
+++ Utils/MappingConfig.cs
@@ -24,6 +24,19 @@ namespace MagicVilla_CouponAPI
                 .ForMember(dest => dest.MaxDistanceNotification, opt => opt.MapFrom(src => src.Category!.MaxDistanceNotification))
                 .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Category!.Description))
                 .ForMember(dest => dest.Instructions, opt => opt.MapFrom(src => src.Category!.Instructions));
+            CreateMap<Incident, IncidentResponseDTO>()
+                .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name))
+                .ForMember(dest => dest.Comments, opt => opt.MapFrom(src => src.Comments))
+                .ForMember(dest => dest.SubmittedByUsername, opt => opt.MapFrom(src => src.User.Username))
+                .ForMember(dest => dest.PhotoURL, opt => opt.MapFrom(src => src.PhotoURL))
+                .ForMember(dest => dest.Latitude, opt => opt.MapFrom(src => src.Latitude))
+                .ForMember(dest => dest.Longitude, opt => opt.MapFrom(src => src.Longitude))
+                .ForMember(dest => dest.SubmittedAt, opt => opt.MapFrom(src => src.SubmittedAt.ToString("yyyy-MM-dd HH:mm:ss")))
+                .ForMember(dest => dest.TotalSubmissions, opt => opt.MapFrom(src => src.TotalSubmissions))
+                .ForMember(dest => dest.State, opt => opt.MapFrom(src => src.State));
+
+
+
         }
     }
 }
